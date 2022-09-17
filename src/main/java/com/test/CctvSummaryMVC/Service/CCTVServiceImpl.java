89d7939cb7270494c2789afc1802a6b54c5bf4f5
Eddi777 <Gjhnfkbot1>
2022-09-17 09:28:01 +0300

    ConcurrentHashMap installed

--- src/main/java/com/test/CctvSummaryMVC/Service/CCTVServiceImpl.java
+++ src/main/java/com/test/CctvSummaryMVC/Service/CCTVServiceImpl.java
@@ -10,9 +10,9 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -28,56 +28,84 @@ public class CCTVServiceImpl {
         this.cctVtoCCTVDTO = cctVtoCCTVDTO;
     }
 
-    public Map<Integer, CCTV> cctvMap = new HashMap<>();
+    private Map<Integer, CCTV> cctvMap = new ConcurrentHashMap<>();
 
     @Value("${app.CCTV_DATA_UPDATE_PERIOD}")
     private long CCTV_DATA_UPDATE_PERIOD; //Duration between update each CCTV, 10 sec
     @Value("${app.CCTV_LIST_UPDATE_PERIOD}")
     private long CCTV_LIST_UPDATE_PERIOD; //Duration between update list of all CCTVs, 60 sec
+    private long cctvLastUpdateTime;
     @Value("${app.START_POINT}")
     private String START_POINT; // = "http://www.mocky.io/v2/5c51b9dd3400003252129fb5"; //start point for update list of all CCTVs
 
     public Set<CCTV> getCCTVList(){
         if (cctvMap.isEmpty()) {
+            firstFillCCTVList();
+        }
+        if ((System.nanoTime() - cctvLastUpdateTime) > CCTV_LIST_UPDATE_PERIOD) {
             updateCCTVList();
         }
         return new HashSet<>(cctvMap.values());
     }
     public Set<CCTVDTO> getCCTVDetails(){
         if (cctvMap.isEmpty()) {
+            firstFillCCTVList();
+        }
+        if ((System.nanoTime() - cctvLastUpdateTime) > CCTV_LIST_UPDATE_PERIOD) {
             updateCCTVList();
-            CCTV newCCTV;
-            for (Integer item: cctvMap.keySet()) {
-                newCCTV = cctvUpdate(cctvMap.get(item));
-                synchronized (cctvMap) {
-                    cctvMap.replace(item, newCCTV);
-                }
+            cctvLastUpdateTime = System.nanoTime();
+        }
+        for (CCTV cctv: cctvMap.values()) {
+            if ((System.nanoTime() - cctv.getLastCheckTime()) > CCTV_DATA_UPDATE_PERIOD ||
+                    cctv.isChanged()) {
+            cctvUpdate(cctv);
+            cctvLastUpdateTime = System.nanoTime();
             }
         }
         return cctvMap.values().stream().
                 map(cctVtoCCTVDTO::cctvToDto).
                 collect(Collectors.toSet());
     }
+
+    private void firstFillCCTVList() {
+        CCTV[] resp = restTemplate.getForObject(
+                START_POINT,
+                CCTV[].class);
+        if (resp == null){
+            System.out.println("Start point is wrong");
+        } else {
+            cctvMap = Arrays.stream(resp).
+                    map(item -> {
+                        item.setChanged(true);
+                        return item;
+                    }).
+                    collect(Collectors.toMap(CCTV::getId, Function.identity()));
+        }
+    }
+
     private void updateCCTVList() {
         CCTV[] resp = restTemplate.getForObject(
                 START_POINT,
                 CCTV[].class);
         if (resp == null){
-            cctvMap.clear();
+            System.out.println("Start point is out of service");
+            cctvLastUpdateTime = System.nanoTime();
         } else {
-            synchronized (cctvMap) {
-                cctvMap = Arrays.stream(resp).
-                        map(item -> {
-                            item.setChanged(true);
-                            return item;
-                        }).
-                        collect(Collectors.toMap(CCTV::getId, Function.identity()));
-            }
+            cctvMap = Arrays.stream(resp).
+                    map(item -> {
+                        item.setChanged(true);
+                        return item;
+                    }).
+                    collect(Collectors.toMap(CCTV::getId, Function.identity()));
         }
     }
 
-    private CCTV cctvUpdate(CCTV cctv) {
-        //get request for SourceDataUrl
+    private void cctvUpdate(CCTV oCCTV) {
+        CCTV cctv = new CCTV();
+        cctv.setId(oCCTV.getId());
+        cctv.setTokenDataUrl(oCCTV.getTokenDataUrl());
+        cctv.setSourceDataUrl(oCCTV.getSourceDataUrl());
+            //get request for SourceDataUrl
         CCTV resp = restTemplate.getForObject(
                 cctv.getSourceDataUrl(),
                 CCTV.class);
@@ -85,7 +113,7 @@ public class CCTVServiceImpl {
             cctv.setUrlType(resp.getUrlType());
             cctv.setVideoUrl(resp.getVideoUrl());
         }
-        //get request for TokenDataUrl
+            //get request for TokenDataUrl
         resp = restTemplate.getForObject(
                 cctv.getTokenDataUrl(),
                 CCTV.class);
@@ -95,8 +123,8 @@ public class CCTVServiceImpl {
         }
         cctv.setLastCheckTime(System.nanoTime());
         cctv.setChanged(false);
-        System.out.println(cctv);
-        System.out.println(resp);
-        return cctv;
+        synchronized (cctvMap) {
+            cctvMap.replace(cctv.getId(), cctv);
+        }
     }
 }
